17149e6ce9398dacf1b0103d490cb7c661a7a002
Tanvir Deol <tanvirdeol31@gmail.com>
2022-12-23 01:13:27 -0500

    range update, range query, much better

--- fenwickTree.cpp
+++ fenwickTree.cpp
@@ -1,56 +1,60 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
+typedef long double ll;
 
-int getSum(int* bitTree, int index) {
-	int sum = 0;
-	while (index > 0) {
-		sum += bitTree[index];
-		index -= index & (-index);
-	}
-	return sum;
+vector<int>B1;
+vector<int>B2;
+int N;
+
+void add(vector<int>&b, int idx, int x) {
+    while (idx <= N) {
+        b[idx] += x;
+        idx += (idx) & (-idx);
+    }
 }
 
-
-void update(int* bitTree, int sz, int index, int val) {
-	index++;
-	while (index <= sz) {
-		bitTree[index] += val;
-		index += index & (-index);
-	}
+// client uses this
+void range_add(int l, int r, int x){
+    add(B1, l, x);
+    add(B1, r+1, -x);
+    add(B2, l, x*(l-1));
+    add(B2, r+1, -x*r);
 }
 
-
-int* construct(int* arr, int sz) {
-
-	int* bitTree = new int[sz + 1];
-	for (int i = 1; i <= sz; i++) {
-		bitTree[i] = 0;
-	}
-
-	for (int i = 0; i < sz; i++) {
-		update(bitTree, sz, i, arr[i]);
-	}
-
-	return bitTree;
+int sum(vector<int>&b, int idx){
+    int total = 0;
+    while (idx > 0) {
+        total += b[idx];
+        idx -= (idx) & (-idx);
+    }
+    return total;
 }
 
+int prefix_sum(int idx){
+    return sum(B1, idx)*idx -  sum(B2, idx);
+}
+
+// client uses this
+int range_sum(int l, int r){
+    return prefix_sum(r) - prefix_sum(l-1);
+}
 
 int main() {
-	int n = 0; cin >> n;
-	int* arr = new int[n];
-
-	for (int i = 0; i < n; i++) {
-		int inp = 0; cin >> inp;
-		arr[i] = inp;
-	}
-
-
-	int* bitTree = construct(arr, n);
-
-	//for (int i = 1; i <= n; i++) {
-	//	cout << bitTree[i] << " ";
-	//}cout << endl;
-	cout << getSum(bitTree, 9) - getSum(bitTree, 5 - 1) << endl;
-
-	return 0;
+    cin >> N;
+    B1.resize(N+1,0);
+    B2.resize(N+1,0);
+    char c; 
+    int l,r,x;
+    while(scanf(" %c",&c) == 1) {
+        if (c == 'A') {
+            scanf("%d %d %d", &l, &r, &x);
+            range_add(l,r,x);
+        } 
+        else if (c == 'S'){
+            scanf("%d %d", &l, &r);
+            cout << range_sum(l, r) << endl;
+        }
+    }
+    return 0;
 }
