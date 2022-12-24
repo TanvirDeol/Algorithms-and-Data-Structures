cc279242192a5e8f78951c4ec1fa8e3a4c234f06
Tanvir Deol <tanvirdeol31@gmail.com>
2022-12-24 14:09:57 -0500

    Update SegTreeLazy.cpp

--- SegTreeLazy.cpp
+++ SegTreeLazy.cpp
@@ -1,82 +1,76 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
-typedef long double ll;
+typedef long long ll;
 
-const int MAXN = 1e3+5;
+const int MAXN = 1e5+5;
 int tree[MAXN*4];
-bool marked[MAXN*4] = {0};
+int lazy[MAXN*4];
 
-void pushup(int pos) {
-    tree[pos] = tree[pos*2] + tree[pos*2 + 1];
-}
-
-void build(int a[], int pos, int tree_l, int tree_r) {
-	if (tree_l == tree_r){
-		tree[pos] = a[tree_l];
-	}  
-	else {
-	    int tree_m = (tree_l + tree_r) / 2;
-	    build(a, pos*2, tree_l, tree_m);
-	    build(a, (pos*2)+1, tree_m + 1, tree_r);
-	    pushup(pos);
-	}
-}
-
-void pushdown(int pos) {
-    if (marked[pos]) {
-        tree[pos*2] = tree[pos*2 + 1] = tree[pos];
-        marked[pos*2] = true;
-        marked[pos*2 + 1] = true;
-        marked[pos] = false;
+void update(int pos, int s, int e, int l, int r, int val) {
+    if (lazy[pos] != 0) {
+        tree[pos] += (e - s + 1) * lazy[pos];
+        if (s != e) {
+            lazy[pos * 2] += lazy[pos];
+            lazy[pos * 2 + 1] += lazy[pos];
+        }
+        lazy[pos] = 0;
     }
-}
-
-void update(int pos, int tree_l, int tree_r, int l, int r, int val) {
-    if (l > r) {
+    if (s > e || s > r || e < l) {
         return;
     }
-    if (l == tree_l && r == tree_r) {
-        tree[pos] = val * (tree_r - tree_l + 1);
-        marked[pos] = true;
+    if (s >= l && e <= r) {
+        seg[pos] += (e - s + 1) * val;
+        if (s != e) {
+            lazy[pos * 2] += val;
+            lazy[pos * 2 + 1] += val;
+        }
+        return;
     }
-    else {
-        pushdown(pos);
-        int tree_m = (tree_l + tree_r) / 2;
-        update(pos*2, tree_l, tree_m, l, min(r, tree_m), val);
-        update(pos*2 + 1, tree_m + 1, tree_r, max(l, tree_m + 1), r, val);
-        pushup(pos);
-    }
+    int mid = s + (e - s)/2;
+    update(pos * 2, s, mid, l, r, val);
+    update(pos * 2 + 1, mid + 1, end, l, r, val);
+    seg[pos] = seg[pos * 2] + seg[pos * 2 + 1];    
 }
 
-int query(int pos, int tree_l, int tree_r, int l, int r) {
-    if (l > tree_r || r < tree_l) {
+int query(int pos, int s, int e, int l, int r) {
+    if (s > e || s > r || e < l) {
         return 0;
     }
-    if (l <= tree_l && r >= tree_r) {
-        return tree[pos];
+    if (lazy[pos] != 0) {
+        tree[pos] += (e - s + 1) * lazy[pos];
+        if (s != e) {
+            lazy[pos * 2] += lazy[pos];
+            lazy[pos * 2 + 1] += lazy[pos];
+        }
+        lazy[pos] = 0;
     }
-    int tree_m = (tree_l + tree_r) / 2;
-    pushdown(pos);
-    return query(pos*2, tree_l, tree_m, l, r) + query(pos*2 + 1, tree_m + 1, tree_r, l, r);
+    if (s >= l && e <= r) {
+        return seg[pos];
+    }
+    int mid = s + (e - s)/2;
+    int p1 = query(pos * 2, s, mid, l, r);
+    int p1 = query(pos * 2 + 1, mid + 1, end, l, r);
+    return p1 + p2;
 }
 
-
 int main() {
-    int a[5] = {1,2,3,4,5};
-    build(a, 1, 0, 5-1);
-    char c;
-    int l, r, val;
-    while(scanf(" %c", &c) == 1) {
-        if (c == 'U') {
-            scanf("%d %d %d", &l, &r, &val);
-            update(1, 0, 5-1, l, r, val);
+    int N,M;
+    cin>>N>>M;
+    vector<int>a(N,0);
+    // populate tree with a[] if needed
+    memset(tree, 0, MAXN*4);
+    memset(marked, false, MAXN*4);
+    int x, l, r, val;
+    while(M--) {
+        cin>>x>>l>>r;
+        if (x == 0) {
+            cin>>val;
+            update(1, 0, N-1, l-1, r-1, val);
         } 
-        else if (c == 'Q') {
-            scanf("%d %d", &l, &r);
-            cout << "SUM: "<< query(1, 0, 5-1, l, r) << endl;
+        else if (x == 1) {
+            printf("%d\n", query(1, 0, N-1, l-1, r-1));
         }
     }
     return 0;
 }
-
