793dcc9f0f7c7e7b221a5ca4c22014e199b5621d
Tanvir Deol <tanvirdeol31@gmail.com>
2022-12-24 14:23:07 -0500

    Update SegTreeLazy.cpp

--- SegTreeLazy.cpp
+++ SegTreeLazy.cpp
@@ -7,12 +7,12 @@ const int MAXN = 1e5+5;
 int tree[MAXN*4];
 int lazy[MAXN*4];
 
-void update(int pos, int s, int e, int l, int r, int val) {
+void update(int pos, int s, int e, int l, int r) {
     if (lazy[pos] != 0) {
-        tree[pos] += (e - s + 1) * lazy[pos];
+        tree[pos] = (e - s + 1) - tree[pos];
         if (s != e) {
-            lazy[pos * 2] += lazy[pos];
-            lazy[pos * 2 + 1] += lazy[pos];
+            lazy[pos * 2] = !lazy[pos * 2];
+            lazy[pos * 2 + 1] = !lazy[pos * 2 + 1];
         }
         lazy[pos] = 0;
     }
@@ -20,17 +20,17 @@ void update(int pos, int s, int e, int l, int r, int val) {
         return;
     }
     if (s >= l && e <= r) {
-        seg[pos] += (e - s + 1) * val;
+        tree[pos] = (e - s + 1) - tree[pos];
         if (s != e) {
-            lazy[pos * 2] += val;
-            lazy[pos * 2 + 1] += val;
+            lazy[pos * 2] = !lazy[pos * 2];
+            lazy[pos * 2 + 1] = !lazy[pos * 2 + 1];
         }
         return;
     }
     int mid = s + (e - s)/2;
-    update(pos * 2, s, mid, l, r, val);
-    update(pos * 2 + 1, mid + 1, end, l, r, val);
-    seg[pos] = seg[pos * 2] + seg[pos * 2 + 1];    
+    update(pos * 2, s, mid, l, r);
+    update(pos * 2 + 1, mid + 1, e, l, r);
+    tree[pos] = tree[pos * 2] + tree[pos * 2 + 1];    
 }
 
 int query(int pos, int s, int e, int l, int r) {
@@ -38,19 +38,19 @@ int query(int pos, int s, int e, int l, int r) {
         return 0;
     }
     if (lazy[pos] != 0) {
-        tree[pos] += (e - s + 1) * lazy[pos];
+        tree[pos] = (e - s + 1) - tree[pos];
         if (s != e) {
-            lazy[pos * 2] += lazy[pos];
-            lazy[pos * 2 + 1] += lazy[pos];
+            lazy[pos * 2] = !lazy[pos * 2];
+            lazy[pos * 2 + 1] = !lazy[pos * 2 + 1];
         }
         lazy[pos] = 0;
     }
     if (s >= l && e <= r) {
-        return seg[pos];
+        return tree[pos];
     }
     int mid = s + (e - s)/2;
     int p1 = query(pos * 2, s, mid, l, r);
-    int p1 = query(pos * 2 + 1, mid + 1, end, l, r);
+    int p2 = query(pos * 2 + 1, mid + 1, e, l, r);
     return p1 + p2;
 }
 
@@ -60,13 +60,12 @@ int main() {
     vector<int>a(N,0);
     // populate tree with a[] if needed
     memset(tree, 0, MAXN*4);
-    memset(marked, false, MAXN*4);
-    int x, l, r, val;
+    memset(lazy, 0, MAXN*4);
+    int x, l, r;
     while(M--) {
         cin>>x>>l>>r;
         if (x == 0) {
-            cin>>val;
-            update(1, 0, N-1, l-1, r-1, val);
+            update(1, 0, N-1, l-1, r-1);
         } 
         else if (x == 1) {
             printf("%d\n", query(1, 0, N-1, l-1, r-1));
